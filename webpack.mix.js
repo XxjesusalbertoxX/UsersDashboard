3f4a737b8549c229c25d2f37394b7e76e209bdaf
XxjesusalbertoxX <villarrealperezjesusalberto@gmail.com>
2025-05-21 06:00:58 -0600

    preparando para despliegue automatico

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,8 +3,8 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .postCss('resources/css/app.css', 'public/css', [])
    .browserSync({
-       proxy: 'http://192.168.123.124:8000',
-       host: '192.168.123.124', // <- usa esta URL exacta
+       proxy: 'http://192.168.1.32:8000',
+       host: '192.168.1.32', // <- usa esta URL exacta
        open: true,                     // <- abrirá el navegador automáticamente
        port: 3000,                     // <- puedes cambiarlo si lo deseas
        files: [
